2885e1d1512e7a9a09bc768de6cb27b65e0cd190
Renan Reis <135069041+Renan-Coding@users.noreply.github.com>
2023-09-27 18:00:08 -0300

    Add files via upload

---
+++ BotaoFujao/script.js
@@ -0,0 +1,15 @@
+function fuja(){
+    var botaonao = document.getElementById('nao')
+
+    var largurajanela = window.innerWidth;
+    var alturajanela = window.innerHeight;
+
+    var maxX = largurajanela - botaonao.offsetWidth
+    var maxY = alturajanela - botaonao.offsetHeight
+
+    var aleatorioX = Math.floor(Math.random() * maxX)
+    var aleatorioY = Math.floor(Math.random() * maxY)
+
+    botaonao.style.left = aleatorioX + "px";
+    botaonao.style.top = aleatorioY + "px";
+}
